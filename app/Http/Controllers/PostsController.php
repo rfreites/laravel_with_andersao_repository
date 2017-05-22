a76d74bf49be5dff37af23f069e9c9986ffe202c
Ronny Freites Vega <rfreites@mybookie.cr>
2017-05-22 15:23:03 -0600

    initial commit

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use Prettus\Validator\Contracts\ValidatorInterface;
+use Prettus\Validator\Exceptions\ValidatorException;
+use App\Http\Requests\PostCreateRequest;
+use App\Http\Requests\PostUpdateRequest;
+use App\Repositories\PostRepository;
+use App\Validators\PostValidator;
+
+
+class PostsController extends Controller
+{
+
+    /**
+     * @var PostRepository
+     */
+    protected $repository;
+
+    /**
+     * @var PostValidator
+     */
+    protected $validator;
+
+    public function __construct(PostRepository $repository, PostValidator $validator)
+    {
+        $this->repository = $repository;
+        $this->validator  = $validator;
+    }
+
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $this->repository->pushCriteria(app('Prettus\Repository\Criteria\RequestCriteria'));
+        $posts = $this->repository->all();
+
+        return response()->json([
+            'data' => $posts,
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  PostCreateRequest $request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(PostCreateRequest $request)
+    {
+
+        try {
+
+            $this->validator->with($request->all())->passesOrFail(ValidatorInterface::RULE_CREATE);
+
+            $post = $this->repository->create($request->all());
+
+            $response = [
+                'message' => 'Post created.',
+                'data'    => $post->toArray(),
+            ];
+
+            return response()->json($response);
+
+        } catch (ValidatorException $e) {
+
+            return response()->json([
+                'error'   => true,
+                'message' => $e->getMessageBag()
+            ]);
+        }
+    }
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $post = $this->repository->find($id);
+
+        return response()->json([
+            'data' => $post,
+        ]);
+    }
+
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return $this->repository->find($id);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  PostUpdateRequest $request
+     * @param  string            $id
+     *
+     * @return Response
+     */
+    public function update(PostUpdateRequest $request, $id)
+    {
+
+        try {
+
+            $this->validator->with($request->all())->passesOrFail(ValidatorInterface::RULE_UPDATE);
+
+            $post = $this->repository->update($request->all(), $id);
+
+            $response = [
+                'message' => 'Post updated.',
+                'data'    => $post->toArray(),
+            ];
+
+
+            return response()->json($response);
+
+        } catch (ValidatorException $e) {
+
+            return response()->json([
+                'error'   => true,
+                'message' => $e->getMessageBag()
+            ]);
+        }
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $deleted = $this->repository->delete($id);
+
+        return response()->json([
+            'message' => 'Post deleted.',
+            'deleted' => $deleted,
+        ]);
+    }
+}
